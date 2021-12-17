62bff7359001bbd3c661177083b33d9b39d330e0
tong-meow <tongshen930@gmail.com>
2021-12-16 16:58:10 -0800

    Add a page for adding a new watching spot

--- app.js
+++ app.js
@@ -14,29 +14,43 @@ db.once("open", () => {
 
 const app = express();
 
-
+// set ejs
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
 
+// GET / : homepage
 app.get('/', (req, res) => {
     res.render('home');
 })
 
+// GET /watchingspots : all owls watching spots index
 app.get('/watchingspots', async (req, res) => {
     const watchingspots = await Watchingspot.find({});
     res.render('watchingspots/index', { watchingspots });
 })
 
+// GET /watchingspots/new : add a new watching spot
 app.get('/watchingspots/new', (req, res) => {
     res.render('watchingspots/new');
 })
 
+// POST /watchingspots : post the new watching spot
+// and redirect to the index page of watching spots
+app.post('/watchingspots', async (req, res) => {
+    const ws = new Watchingspot(req.body.watchingspot);
+    await ws.save();
+    res.redirect(`/watchingspots/${ws._id}`);
+})
+
+// GET /watchingspots/:id : view one watching spot's information page
 app.get('/watchingspots/:id', async(req, res) => {
     const watchingspot = await Watchingspot.findById(req.params.id);
     res.render('watchingspots/show', { watchingspot });
 })
 
+// start listening on localhost port 3000
 app.listen(3000, () => {
     console.log("Serving on port 3000.");
 })
