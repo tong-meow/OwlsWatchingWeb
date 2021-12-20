8ac0465aafd9c755151d3025224db56c5d6358f6
tong-meow <tongshen930@gmail.com>
2021-12-19 23:55:52 -0800

    Add some basic error handlers, e.g. required message is missing for post and put, or 404 not found.

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { wsSchema } = require('./schemas.js');
+const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Watchingspot = require('./models/watchingspot');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/owls-watch');
 
@@ -24,16 +27,27 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+// check the validation for post and put
+const validateWS = (req, res, next) => {
+    const { error } = wsSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    } else {
+        next();
+    }
+}
+
 // GET / : homepage
 app.get('/', (req, res) => {
     res.render('home');
 })
 
 // GET /watchingspots : all owls watching spots index
-app.get('/watchingspots', async (req, res) => {
+app.get('/watchingspots', catchAsync(async (req, res) => {
     const watchingspots = await Watchingspot.find({});
     res.render('watchingspots/index', { watchingspots });
-})
+}))
 
 // GET /watchingspots/new : add a new watching spot
 app.get('/watchingspots/new', (req, res) => {
@@ -42,37 +56,54 @@ app.get('/watchingspots/new', (req, res) => {
 
 // POST /watchingspots : post the new watching spot
 // and redirect to the index page of watching spots
-app.post('/watchingspots', async (req, res) => {
+app.post('/watchingspots', validateWS, catchAsync(async (req, res) => {
+    // handle error if the body doesn't exist
+    // if (!req.body.watchingspot) throw new ExpressError('Invalid Watchingspot Data', 400);
     const ws = new Watchingspot(req.body.watchingspot);
     await ws.save();
     res.redirect(`/watchingspots/${ws._id}`);
-})
+}))
 
 // GET /watchingspots/:id : view one watching spot's information page
-app.get('/watchingspots/:id', async(req, res) => {
+app.get('/watchingspots/:id', catchAsync(async (req, res) => {
     const ws = await Watchingspot.findById(req.params.id);
     res.render('watchingspots/show', { ws });
-})
+}))
 
 // GET /watchingspots/:id/edit : edit page to a watching spot
-app.get('/watchingspots/:id/edit', async(req, res) => {
+app.get('/watchingspots/:id/edit', catchAsync(async (req, res) => {
     const ws = await Watchingspot.findById(req.params.id);
     res.render('watchingspots/edit', { ws });
-})
+}))
 
 // PUT /watchingspots/:id : submit edit
-app.put('/watchingspots/:id', async(req, res) => {
+app.put('/watchingspots/:id', validateWS, catchAsync(async (req, res) => {
     const { id } = req.params;
     const ws = await Watchingspot.findByIdAndUpdate(id, { ...req.body.watchingspot });
     res.redirect(`/watchingspots/${ws._id}`);
-})
+}))
 
 // DELETE /watchingspots/:id : submit delete request
 // and redirect to the index page of watching spots
-app.delete('/watchingspots/:id', async(req, res) => {
+app.delete('/watchingspots/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await Watchingspot.findByIdAndDelete(id);
     res.redirect('/watchingspots');
+}))
+
+// handle 404 not found error
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+})
+
+// handle other errors (e.g. ID not found)
+app.use((err, req, res, next) => {
+    // 500 and something went wrong is default code and message
+    const { statusCode = 500 } = err;
+    if (!err.message || statusCode === 500) {
+        err.message = "Oops! Something went wrong :("
+    }
+    res.status(statusCode).render('error', { err });
 })
 
 // start listening on localhost port 3000
