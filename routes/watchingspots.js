3e366a029a770add8d583cace4ec91802b685e1a
tong-meow <tongshen930@gmail.com>
2022-01-14 02:25:44 -0700

    Add basic user authentication to the project

--- routes/watchingspots.js
+++ routes/watchingspots.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Watchingspot = require('../models/watchingspot');
 const { wsSchema } = require('../schemas.js');
+const { isLoggedIn } = require('../middleware');
 
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
@@ -27,13 +28,13 @@ router.get('/', catchAsync(async (req, res) => {
 }))
 
 // GET /watchingspots/new : create a new watching spot
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('watchingspots/new');
 })
 
 // POST /watchingspots : post the new watching spot
 // and redirect to the index page of watching spots
-router.post('/', validateWS, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateWS, catchAsync(async (req, res) => {
     // handle error if the body doesn't exist
     // if (!req.body.watchingspot) throw new ExpressError('Invalid Watchingspot Data', 400);
     const ws = new Watchingspot(req.body.watchingspot);
@@ -48,26 +49,26 @@ router.get('/:id', catchAsync(async (req, res) => {
     // if the id is not found, redirect to the index page
     // flash the error message
     if(!ws){
-        req.flash('error', 'Oops, this watching spot is not found.');
+        req.flash('error', 'Oops, this watching spot is not found');
         return res.redirect('/watchingspots');
     }
     res.render('watchingspots/show', { ws });
 }))
 
 // GET /watchingspots/:id/edit : edit page of a watching spot
-router.get('/:id/edit', catchAsync(async (req, res) => {
+router.get('/:id/edit', isLoggedIn, catchAsync(async (req, res) => {
     const ws = await Watchingspot.findById(req.params.id);
     // if the id is not found, redirect to the index page
     // flash the error message
     if(!ws){
-        req.flash('error', 'Oops, this watching spot is not found.');
+        req.flash('error', 'Oops, this watching spot is not found');
         return res.redirect('/watchingspots');
     }
     res.render('watchingspots/edit', { ws });
 }))
 
 // PUT /watchingspots/:id : submit edit
-router.put('/:id', validateWS, catchAsync(async (req, res) => {
+router.put('/:id', isLoggedIn, validateWS, catchAsync(async (req, res) => {
     const { id } = req.params;
     const ws = await Watchingspot.findByIdAndUpdate(id, { ...req.body.watchingspot });
     req.flash('success', 'Thank you! The watching spot is successufully updated!')
@@ -76,10 +77,10 @@ router.put('/:id', validateWS, catchAsync(async (req, res) => {
 
 // DELETE /watchingspots/:id : submit delete request
 // and redirect to the index page of watching spots
-router.delete('/:id', catchAsync(async (req, res) => {
+router.delete('/:id', isLoggedIn, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Watchingspot.findByIdAndDelete(id);
-    req.flash('success', 'The watching spot is successufully deleted!')
+    req.flash('success', 'The watching spot is successufully deleted')
     res.redirect('/watchingspots');
 }))
 
